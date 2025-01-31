31a92d61d2256cb2ca9efa6ff3ae3abb776f1ae2
Coralie RUIZ <coralie.ruiz@tohero.fr>
2025-01-31 10:11:04 +0100

    refreshPonies() + styling <li> of ponies

--- src/app/ns-ponies/ns-ponies.component.ts
+++ src/app/ns-ponies/ns-ponies.component.ts
@@ -12,4 +12,15 @@ export class NsPoniesComponent {
     { id: 1, name: 'Rainbow Dash' },
     { id: 2, name: 'Pinkie Pie' }
   ]);
+
+  refreshPonies() {
+    if(this.ponies().length < 4) {
+      this.ponies.set([
+        ...this.ponies(),
+        { id: 3, name: 'Fluttershy' },
+        { id: 4, name: 'Rarity' }
+      ]);
+    }
+  }
 }
+
---
+++ src/app/races/races.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pr-races',
+  imports: [],
+  templateUrl: './races.component.html',
+  styleUrl: './races.component.css'
+})
+export class RacesComponent {
+
+}
